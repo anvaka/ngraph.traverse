1bb2f4f8c87d2835748bba6f5541ba93d49dd4ca
anvaka <anvaka@gmail.com>
2013-10-27 01:57:15 -0700

    Links now accept arrays/iterators in from()/to()

--- test/links.js
+++ test/links.js
@@ -10,15 +10,14 @@ test('get all links', function(t) {
   var expectedLinksCount = graph.getLinksCount();
   var visitedCount = 0;
 
-  debugger;
   traverse(graph)
       .links()
       .forEach(function(link){
-        t.ok(link, "link should be passed as an argument");
+        t.ok(link, 'link should be passed as an argument');
         visitedCount += 1;
       });
 
-  t.equal(visitedCount, expectedLinksCount, "Number of edges is not correct");
+  t.equal(visitedCount, expectedLinksCount, 'Number of edges is not correct');
   t.end();
 });
 
@@ -34,12 +33,12 @@ test('get all links from node', function(t) {
       .links()
       .from(startNodeId)
       .forEach(function(link){
-        t.ok(link, "link should be passed as an argument");
-        t.equal(link.fromId, startNodeId, "from node id is not correct");
+        t.ok(link, 'link should be passed as an argument');
+        t.equal(link.fromId, startNodeId, 'from node id is not correct');
         visitedCount += 1;
       });
 
-  t.equal(visitedCount, expectedLinksCount, "Number of edges is not correct");
+  t.equal(visitedCount, expectedLinksCount, 'Number of edges is not correct');
   t.end();
 });
 
@@ -55,80 +54,94 @@ test('get all links to node', function(t) {
       .links()
       .to(endNodeId)
       .forEach(function(link){
-        t.ok(link, "link should be passed as an argument");
-        t.equal(link.toId, endNodeId, "end node id is not correct");
+        t.ok(link, 'link should be passed as an argument');
+        t.equal(link.toId, endNodeId, 'end node id is not correct');
         visitedCount += 1;
       });
 
-  t.equal(visitedCount, expectedLinksCount, "Number of edges is not correct");
+  t.equal(visitedCount, expectedLinksCount, 'Number of edges is not correct');
   t.end();
 });
 
 test('get filtered links from node', function(t) {
   var graph = createGraph();
-  graph.addLink(0, 1, "Lu");
-  graph.addLink(1, 2, "Lu");
-  graph.addLink(0, 2, "Tako");
+  graph.addLink(0, 1, 'Lu');
+  graph.addLink(1, 2, 'Lu');
+  graph.addLink(0, 2, 'Tako');
 
   var visitedCount = 0;
   traverse(graph)
       .links()
       .from(0)
-      .where(function(link) { return link.data === "Lu"; })
+      .where(function(link) { return link.data === 'Lu'; })
       .forEach(function(link){
-        t.ok(link.data, "link should have associated data with it");
-        t.equal(link.data, "Lu");
+        t.ok(link.data, 'link should have associated data with it');
+        t.equal(link.data, 'Lu');
         visitedCount += 1;
       });
 
-  t.equal(visitedCount, 1, "Only one link should be visited");
+  t.equal(visitedCount, 1, 'Only one link should be visited');
   t.end();
 });
 
 test('get all links filtered', function(t) {
   var graph = createGraph();
-  graph.addLink(0, 1, "Lu");
-  graph.addLink(1, 2, "Lu");
-  graph.addLink(0, 2, "Tako");
+  graph.addLink(0, 1, 'Lu');
+  graph.addLink(1, 2, 'Lu');
+  graph.addLink(0, 2, 'Tako');
 
   var visitedCount = 0;
   traverse(graph)
       .links()
-      .where(function (link) { return link.data === "Lu"; })
+      .where(function (link) { return link.data === 'Lu'; })
       .forEach(function (link){
-        t.ok(link.data, "link should have associated data with it");
-        t.equal(link.data, "Lu");
+        t.ok(link.data, 'link should have associated data with it');
+        t.equal(link.data, 'Lu');
         visitedCount += 1;
       });
 
-  t.equal(visitedCount, 2, "Two links should be visited");
+  t.equal(visitedCount, 2, 'Two links should be visited');
   t.end();
 });
 
-/*
 test('get all links to neighbors', function (t) {
   var graph = createGraph();
-  // a small binary tree:
+  // a small graph:
+  //    0 -> 1
+  //       2   3
+  //     4
   graph.addLink(0, 1);
   graph.addLink(1, 2); graph.addLink(1, 3);
   graph.addLink(2, 4);
 
-  t.test("visit first level from root", function (t) {
+  t.test('visit custom set of to nodes', function (t) {
     var startNodeId = 0;
     var visitedCount = 0;
 
     traverse(graph)
       .links()
-      .from(startNodeId)
-      .to()
+      .to([2, 3])
       .forEach(function (link) {
         visitedCount += 1;
-        t.ok(link, "Link should be present");
+        t.ok(link.toId === 2 || link.toId === 3, 'Link should go to either 2 or 3');
       });
-    t.equal(visitedCount, 1, "Should visit four neighbors");
+    t.equal(visitedCount, 2, 'Unexpected number of links visited');
     t.end();
   });
 
-});
+  t.test('visit custom set of from nodes', function (t) {
+    var startNodeId = 0;
+    var visitedCount = 0;
 
-*/
+    traverse(graph)
+      .links()
+      .from([1, 2])
+      .forEach(function (link) {
+        visitedCount += 1;
+        t.ok(link.fromId === 1 || link.fromId === 2, 'Link should go to either 2 or 3');
+      });
+    t.equal(visitedCount, 3, 'Unexpected number of links visited');
+    t.end();
+  });
+  t.end();
+});
