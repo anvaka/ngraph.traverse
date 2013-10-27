dc87f27dd0cb39965b2069d451118354b180831e
anvaka <anvaka@gmail.com>
2013-10-26 21:48:51 -0700

    node().neighbors() accepts iterators

--- test/nodes.js
+++ test/nodes.js
@@ -19,10 +19,10 @@ test('Visit all nodes', function (t) {
 });
 
 test('Visit all neighbors', function (t) {
-  var g = createCompleteGraph(5);
+  var g = createBinTree(5);
 
   var visitedNodes = 0;
-  var startFrom = 0;
+  var startFrom = 1;
   traverse(g)
     .nodes()
     .neighbors(startFrom)
@@ -31,9 +31,41 @@ test('Visit all neighbors', function (t) {
       visitedNodes++;
     });
 
+  t.equal(visitedNodes, 2, "Expected to visit 2 nodes");
+  t.end();
+});
+
+test('Visit all grandchildren', function (t) {
+  var g = createBinTree(5);
+
+  var visitedNodes = 0;
+  var startFrom = 1;
+  var nodes = traverse(g).nodes();
+  var children = nodes.neighbors(startFrom);
+  var grandChildren = nodes.neighbors(children);
+
+  grandChildren.forEach(function (node) {
+      t.notEqual(startFrom, node.id);
+      visitedNodes++;
+    });
+
   t.equal(visitedNodes, 4, "Expected to visit 4 nodes");
   t.end();
 });
-/*
 
-*/
+function createBinTree(depth) {
+  var g = createGraph(),
+      count = Math.pow(2, depth),
+      level;
+
+  for (level = 1; level < count; ++level) {
+      var root = level,
+          left = root * 2,
+          right = root * 2 + 1;
+
+      g.addLink(root, left);
+      g.addLink(root, right);
+  }
+
+  return g;
+}
